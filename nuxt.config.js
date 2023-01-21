39711cff9457a45d59469e1b3834a00c12f56521
José Victor <josevictordev@gmail.com>
2023-01-21 11:31:33 -0300

    feat: reset project

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio-v5-nuxt',
+    title: 'José Victor - Front-end Developer',
     htmlAttrs: {
       lang: 'en',
     },
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/sass/base/_reset.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial />
+  <h1>Portfólio</h1>
 </template>
 
 <script>
@@ -7,3 +7,5 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss" scoped></style>
