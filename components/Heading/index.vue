3d8d692a3fb8e4409dee4d672ec1834461bd1f36
José Victor <josevictordev@gmail.com>
2023-01-31 14:00:36 -0300

    feat: component -> Heading

---
+++ components/Heading/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <Bars />
+    <h3 class="heading">{{ title }}</h3>
+    <Bars reverse />
+  </header>
+</template>
+
+<script>
+import Bars from '@/components/Bars'
+
+export default {
+  name: 'Heading',
+  components: {
+    Bars,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.heading {
+  color: var(--green);
+  margin-bottom: var(--m-8);
+}
+</style>
