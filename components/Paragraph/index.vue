e83b7bce64595f6c65c580c4fd4390de3af225f4
José Victor <josevictordev@gmail.com>
2023-01-31 21:11:30 -0300

    feat: component -> Paragraph

---
+++ components/Paragraph/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="paragraph"><slot /></p>
+</template>
+
+<script>
+export default {
+  name: 'Paragraph',
+}
+</script>
+
+<style lang="scss" scoped>
+.paragraph {
+  font-size: var(--fs-14);
+  line-height: var(--fs-18);
+
+  @media (max-wight: 768px) {
+    font-size: var(--fs-12);
+  }
+}
+</style>
