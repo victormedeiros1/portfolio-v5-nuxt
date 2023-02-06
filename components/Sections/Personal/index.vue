8358aa23bbd7347f0f9664560d1c228edfb0d12b
José Victor <josevictordev@gmail.com>
2023-02-05 23:06:35 -0300

    feat: personal projects text

--- components/Sections/Personal/index.vue
+++ components/Sections/Personal/index.vue
@@ -51,27 +51,12 @@ export default {
             {
               id: 0,
               paragraph:
-                "Tudum is an international event that announces several news about Netflix projects. In 2021, the 3rd edition took place to celebrate 10 years of Netflix in Brazil. Almanac Tudum was launched together with the event, it is a book with exclusive material about the company's productions.",
+                'This project brought me big changes. The goal was to experiment with new technologies and play with Redux in practice, as the need to manage the state of my applications in a more organized and scalable way was already knocking the door.',
             },
             {
               id: 1,
               paragraph:
-                "Hox.rs was responsible for implementing a shorter “virtual version” of Almanac Tudum. Through Figma we had access to the project screens and started development. I worked as a frontend developer creating the code for most of the site's pages.",
-            },
-            {
-              id: 2,
-              paragraph:
-                "As it was my first time working in a company, I couldn't believe the moment I saw who the project was for. When I entered Figma and saw that someone from Netflix was working there “on my side”, the feeling was of euphoria.",
-            },
-            {
-              id: 3,
-              paragraph:
-                'Working together with a team of developers, learning from them and creating something for such a big name was wonderful.',
-            },
-            {
-              id: 4,
-              paragraph:
-                "Of all the projects I've participated so far, this is without a doubt the one I love the most, because it was where it all started and where I was sure it was in this world that I wanted to work.",
+                "When it came time to list what would be needed to create the application, I thought it would be cool to use some tools different from the ones I usually use to increase the range of possibilities, Vite and Stitches are some examples, it was a very interesting process, it's good to get out of the zone of comfort.",
             },
           ],
         },
@@ -86,27 +71,11 @@ export default {
             {
               id: 0,
               paragraph:
-                'A project to celebrate the content creators who managed to reach the Top 10 Podcasts in Brazil.',
+                'The idea of creating my own music player was very exciting and after a few projects I realized that I was ready to start this old idea. This was my first personal project combining TypeScript with Redux, what a rewarding experience.',
             },
             {
               id: 1,
-              paragraph:
-                'I participated in two stages in this project, first in the development of the page where I built much of the body of the site, we accessed the data from an endpoint and made the listing showing its information.',
-            },
-            {
-              id: 2,
-              paragraph:
-                "The second stage was more interesting and challenging, I had never worked with email marketing and it seemed to me to be something very simple, until I discovered the limitations at the code level, for example, the structure of the email had to be made only with tables, the css all inline , some css attributes and html tags didn't work, the images would have to be placed in the cloud for display and several other limitations that I discovered over time.",
-            },
-            {
-              id: 3,
-              paragraph:
-                'Studying further I found that each email service can interpret its code in a different way, a css attribute could work in Gmail but not in Outlook. So it was necessary to do several tests.',
-            },
-            {
-              id: 4,
-              paragraph:
-                'Again I was shocked to learn that the project was for none other than Spotify and even more shocked when I went to implement the emails and realized that they would be sent to podcasts that I listened to daily, to the biggest in the country. It was a mixture of endless happiness and fear, there was a big responsibility there, I never did so many tests in my life until I was sure that everything was perfect hahahaha.',
+              paragraph: `Soung Glory is special to me, while working on it I found that each project transmits a different energy. Everything I've worked on until today has always involved only screens, when I clicked on "Play" for the first time and heard the music play, I don't know how to explain it, I felt that I was progressing in my journey and producing something different, that feeling is priceless.`,
             },
           ],
         },
