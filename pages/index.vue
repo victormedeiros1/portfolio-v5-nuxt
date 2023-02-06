58e459fa24d4a8a379ed58fcdc63e53ffb2ebdb5
José Victor <josevictordev@gmail.com>
2023-02-06 19:05:01 -0300

    refactor: removed font archives and use GoogleFonts link

--- pages/index.vue
+++ pages/index.vue
@@ -33,6 +33,20 @@ export default {
   },
   head: {
     title: 'José Victor | Frontend Developer',
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Concert+One&display=swap',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700;900&display=swap',
+      },
+    ],
     meta: [
       {
         hid: 'og:title',
