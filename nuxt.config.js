2edd3cdc8d745f16ce65fe3d848f788a87338600
José Victor <josevictordev@gmail.com>
2023-02-08 23:13:09 -0300

    feat: enable ssr,target static and remove dist gitignore

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -70,5 +73,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    publicPath: '/_jvd/',
+  },
 }
